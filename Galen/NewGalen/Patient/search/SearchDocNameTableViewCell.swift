76d81f61e9befd332f3f121da9907bc7e77b4b26
Mohamed_Elfakharany <mohmedelfakharany@gmail.com>
2019-07-17 14:44:47 +0200

    NewDesign2ndVersion3

---
+++ Galen/NewGalen/Patient/search/SearchDocNameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchDocNameTableViewCell.swift
+//  Galen
+//
+//  Created by elfakharany on 7/17/19.
+//  Copyright © 2019 Mohamed Elfakharany. All rights reserved.
+//
+
+import UIKit
+
+class SearchDocNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Galen/NewGalen/Patient/search/SearchDocNameVC2.swift
@@ -0,0 +1,79 @@
+//
+//  SearchDocNameVC2.swift
+//  Galen
+//
+//  Created by elfakharany on 7/17/19.
+//  Copyright © 2019 Mohamed Elfakharany. All rights reserved.
+//
+
+import UIKit
+
+class SearchDocNameVC2: UIViewController ,UITableViewDataSource , UITableViewDelegate {
+    
+    var DocsArray:[String] = []
+    var SelectedDocs:[String] = []
+    var searching = false
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var DocNameSearch: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return SelectedDocs.count
+        }else {
+            return DocsArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let MyCell = tableView.dequeueReusableCell(withIdentifier: "GovesCell", for: indexPath) as? InsuranceCompanyTableViewCell
+        
+    //    MyCell?.inittiateCell(image: #imageLiteral(resourceName: "calendar-times") , label: DocsArray[indexPath.row].name! )
+        return MyCell!
+    }
+    
+    // keybord down
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+}
+
+extension SearchDocNameVC2: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //  SelectedSpetiality = SpecailityArray.filter{$0.name?.range(of: searchText, options: [.anchored, .caseInsensitive]) != nil }
+        SelectedDocs.removeAll()
+        for insurance in DocsArray {
+//            if insurance.name?.contains(searchText) ?? false{
+//                SelectedDocs.append(insurance)
+//                print(SelectedDocs)
+//                self.tableView.reloadData()
+//            }
+            searching = true
+            self.tableView.reloadData()
+        }
+        
+        tableView.reloadData()
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        searchBar.text = ""
+        tableView.reloadData()
+    }
+}
