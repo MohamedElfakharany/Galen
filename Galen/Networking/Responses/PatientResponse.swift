7ed92074dd0a9da1973e80cad5bba8db58f42e64
Eslam <binshakerr@gmail.com>
2019-07-26 00:03:34 +0200

    start of network layer

---
+++ Galen/Networking/Responses/PatientResponse.swift
@@ -0,0 +1,9 @@
+//
+//  PatientResponse.swift
+//  Galen
+//
+//  Created by Eslam Shaker  on 7/25/19.
+//  Copyright © 2019 Mohamed Elfakharany. All rights reserved.
+//
+
+import Foundation
---
+++ Galen/Networking/Services/PatientService.swift
@@ -0,0 +1,12 @@
+//
+//  PatientService.swift
+//  Galen
+//
+//  Created by Eslam Shaker  on 7/25/19.
+//  Copyright © 2019 Mohamed Elfakharany. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+
